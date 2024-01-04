046cf7b1656988c16f61f00e50050daa171b1841
winglechen <winglechen@gmail.com>
2024-01-04 11:15:23 +0800

    feature: add empty NettyRPCServer NettyRPCClient

---
+++ main/src/java/study/daydayup/minrpc/netty/NettyRPCClient.java
@@ -0,0 +1,6 @@
+package study.daydayup.minrpc.netty;
+
+import study.daydayup.minrpc.api.RPCClient;
+
+public class NettyRPCClient implements RPCClient {
+}
---
+++ main/src/java/study/daydayup/minrpc/netty/NettyRPCServer.java
@@ -0,0 +1,6 @@
+package study.daydayup.minrpc.netty;
+
+import study.daydayup.minrpc.api.RPCServer;
+
+public class NettyRPCServer implements RPCServer {
+}
